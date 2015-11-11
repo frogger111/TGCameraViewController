206cef34c5770a0f5c17364cab16b3a05700b335
Tobiasz Parys <TobiaszParys@MacBook-Pro-MC0001.local>
2015-11-11 21:48:56 +0100

    Fork test

---
+++ TGCameraViewController/Classes/Test/TGTest.m
@@ -0,0 +1,13 @@
+//
+//  TGTest.m
+//  TGCameraViewController
+//
+//  Created by Tobiasz Parys on 11/11/15.
+//  Copyright © 2015 Tudo Gostoso Internet. All rights reserved.
+//
+
+#import "TGTest.h"
+
+@implementation TGTest
+
+@end
